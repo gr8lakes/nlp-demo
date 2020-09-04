a2f7e026307a94fa60042653533952db49d24dbb
gr9lakes <chen_tian@outlook.com>
2020-09-04 16:41:59 +0800

    udpate

--- workflow_demo.py
+++ workflow_demo.py
@@ -22,7 +22,7 @@ def query(id, client):
         use_query_cache=False,
         query_parameters=[
             bigquery.ScalarQueryParameter("id", "INT64", id)
-            ]   
+        ]
     )
 
     sql = """
@@ -30,7 +30,7 @@ def query(id, client):
         FROM `webeye-internal-test.cloud_test.origin`
         WHERE id = @id
         """
-    query_job = client.query(sql, job_config=job_config) 
+    query_job = client.query(sql, job_config=job_config)
 
     for row in query_job:
         id = row["id"]
@@ -47,11 +47,11 @@ def query(id, client):
 
 # Translate the comment to English via Translate API v3.
 def translate_text(client, project_id, text):
-    parent = client.location_path(project_id, "global")
+    parent = "projects/" + project_id + "/locations/global"
     response = client.translate_text(
         parent=parent,
         contents=[text],
-        mime_type="text/plain",  # mime types: text/plain, text/html
+        mime_type="text/plain",
         target_language_code="en",
     )
     for translation in response.translations:
@@ -63,10 +63,13 @@ def translate_text(client, project_id, text):
     translate_data["source_language"] = source_language
     return translate_data
 
+
 def inline_text_payload(content):
-    return {'text_snippet': {'content': content, 'mime_type': 'text/plain'} }
+    return {'text_snippet': {'content': content, 'mime_type': 'text/plain'}}
 
 # Use the custom model to predict the comment content.
+
+
 def classification_analyze(client, text):
     project_id = "839062387451"
     model_id = "TCN2731224266790928384"
@@ -76,12 +79,14 @@ def classification_analyze(client, text):
     args = parser.parse_args()
 
     payload = inline_text_payload(args.text_content)
-    model_full_id = prediction_client.model_path(project_id, "us-central1", model_id)
-            
+    model_full_id = prediction_client.model_path(
+        project_id, "us-central1", model_id)
+
     response = prediction_client.predict(model_full_id, payload)
 
     topic = response.payload[0].display_name
-    score = format(Decimal(response.payload[0].classification.score).quantize(Decimal('0.00')))    
+    score = format(
+        Decimal(response.payload[0].classification.score).quantize(Decimal('0.00')))
 
     classification_data = {}
     classification_data["classification_topic"] = topic
@@ -89,8 +94,10 @@ def classification_analyze(client, text):
     return classification_data
 
 # Use NLP API to analyze the comment contents.
+
+
 def annotateText(client, text):
-    document = { "content": text, "type": "PLAIN_TEXT", "language": "en" }
+    document = {"content": text, "type": "PLAIN_TEXT", "language": "en"}
     # Available values: NONE, UTF8, UTF16, UTF32
     encoding_type = enums.EncodingType.UTF8
 
@@ -98,7 +105,8 @@ def annotateText(client, text):
         "extract_document_sentiment": True,
         "extract_entity_sentiment": True
     }
-    response = client.annotate_text(document=document, features=features, encoding_type=encoding_type)
+    response = client.annotate_text(
+        document=document, features=features, encoding_type=encoding_type)
 
     data = {}
 
@@ -140,11 +148,13 @@ def annotateText(client, text):
         data["entity_salience"] = list_entity_salience
         data["entity_sentiment_score"] = list_sentiment_score
         data["entity_sentiment_magnitude"] = list_sentiment_magnitude
-    
+
     return data
+
 
 def convert_json(text):
     return json.dumps(text)
+
 
 def upload_to_bucket(bucket_name, source_file_name, destination_blob_name):
     # Instantiates a client
@@ -165,11 +175,12 @@ def upload_to_bucket(bucket_name, source_file_name, destination_blob_name):
             source_file_name, destination_blob_name
         )
     )
-
+    return('gs://' + bucket_name + '/' + destination_blob_name)
+    
 # Import all the returned results into the new table of BigQuery.
-def import_to_bq(client, dataset_id, table_id, filename):
-    dataset_ref = client.dataset(dataset_id)
-    table_ref = dataset_ref.table(table_id)
+def import_to_bq(client, table_id, uri):
+    # TODO(developer): Set table_id to the ID of the table to create.
+    # table_id = "your-project.your_dataset.your_table_name"
     job_config = bigquery.LoadJobConfig()
 
     job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
@@ -180,45 +191,64 @@ def import_to_bq(client, dataset_id, table_id, filename):
         bigquery.SchemaField("pub_time", "DATETIME", mode="NULLABLE"),
         bigquery.SchemaField("translation_text", "STRING", mode="NULLABLE"),
         bigquery.SchemaField("source_language", "STRING", mode="NULLABLE"),
-        bigquery.SchemaField("classification_topic", "STRING", mode="NULLABLE"),
+        bigquery.SchemaField("classification_topic",
+                             "STRING", mode="NULLABLE"),
         bigquery.SchemaField("classification_score", "FLOAT", mode="NULLABLE"),
         bigquery.SchemaField("sentiment_score", "FLOAT", mode="NULLABLE"),
         bigquery.SchemaField("sentiment_magnitude", "FLOAT", mode="NULLABLE"),
         bigquery.SchemaField("entity_name", "STRING", mode="REPEATED"),
         bigquery.SchemaField("entity_type", "STRING", mode="REPEATED"),
         bigquery.SchemaField("entity_salience", "FLOAT", mode="REPEATED"),
-        bigquery.SchemaField("entity_sentiment_score", "FLOAT", mode="REPEATED"),
-        bigquery.SchemaField("entity_sentiment_magnitude", "FLOAT", mode="REPEATED"),
+        bigquery.SchemaField("entity_sentiment_score",
+                             "FLOAT", mode="REPEATED"),
+        bigquery.SchemaField("entity_sentiment_magnitude",
+                             "FLOAT", mode="REPEATED"),
     ]
 
-    with open(filename, "rb") as source_file:
-        job = client.load_table_from_file(source_file, table_ref, job_config=job_config)
+    load_job = client.load_table_from_uri(
+        uri,
+        table_id,
+        location="US",  # Must match the destination dataset location.
+        job_config=job_config,
+    )
+
+    # with open(filename, "rb") as source_file:
+    #    job = client.load_table_from_file(source_file, table_ref, job_config=job_config)
 
     # Waits for table load to complete.
-    job.result()
+    load_job.result()
+    destination_table = client.get_table(table_id)
 
-    print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
+    print("Loaded {} rows.".format(destination_table.num_rows))
+
+    # print("Loaded {} rows into {}:{}.".format(load_job.output_rows, dataset_id, table_id))
     return 0
 
+
 if __name__ == '__main__':
-    g = open('analyze_results.json', 'a')
+    real_time = time.strftime("%Y%m%d%H%M%S", time.localtime())
+
+    file = 'analyze_results' + real_time + '.json'
+    g = open(file, 'a')
     bq_client = bigquery.Client()
     prediction_client = automl.PredictionServiceClient()
     translate_client = translate.TranslationServiceClient()
     nlp_client = language_v1.LanguageServiceClient()
+
     project_id = 'webeye-internal-test'
+    dataset_id = 'support_shein'
 
     try:
         for id in range(1, 10):
             comment_data = query(id, bq_client)
             comment = comment_data["comment"]
 
-            translate_data = translate_text(translate_client, project_id, comment)
-            classification_data = classification_analyze(prediction_client, translate_data["translation_text"])
-            document_entities_data = annotateText(nlp_client, translate_data["translation_text"])
-            analyze_results = {**comment_data, **translate_data, **classification_data, **document_entities_data} 
+            translate_data = translate_text( translate_client, project_id, comment )
+            classification_data = classification_analyze( prediction_client, translate_data["translation_text"] )
+            document_entities_data = annotateText( nlp_client, translate_data["translation_text"] )
+            analyze_results = { **comment_data, **translate_data, **classification_data, **document_entities_data }
             print(analyze_results)
-            
+
             analyze_results_json = convert_json(analyze_results)
             g.write(analyze_results_json + '\n')
             time.sleep(0.5)
@@ -227,10 +257,12 @@ if __name__ == '__main__':
         print("Error {}".format(err))
     finally:
         g.close()
-        
-        # upload file to gcs
-        upload_to_bucket("test-chent-we/shein", "analyze_results.json", "analyze_results.json")
-        
-        #print("Import to BigQuery Table!")
-        #import_to_bq(bq_client, "cloud_test", "analyze_data_all", "analyze_results.json")
+
+        # upload file to GCS
+        uri = upload_to_bucket("test-chent-we", file, file)
+
+        print("Import to BigQuery Table from json file in GCS")
+        table = project_id + '.' + dataset_id + '.' + 'analyze_result' + real_time
+
+        import_to_bq(bq_client, table, uri)
         print("finished!")
